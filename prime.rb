e33abcd6ef4fa8f690d9c7d841e0c01db6c55a08
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 00:02:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,31 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+  # check if int is pos/neg
+  if num <= 1
+    false
+  # check if int is equal to 2 or 3
+  elsif num == 2 || num == 3
+    true
+  else
+# range is inclusive of all numbers from 2 to num/2
+    range = (2..num/2).to_a
+  
+# loop through range, check if num is divisible by any number, use num % number == 0 to check  
+# if divisible by number, return false
+# after condition fails for all numbers, return true
+
+    range.each do |el|
+      puts "current element is #{el}"
+      if num % el == 0
+        return false
+      else 
+        range.delete_if{|other_el| other_el % el == 0}
+        puts range
+      binding.pry
+      end
+    end
+    return true
+  end
+end
+
+prime?(18899)
